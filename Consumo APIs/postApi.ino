ab44507e6ebf0d30986ac2b979083c2b07790076
francisco-ag <francisamado80@gmail.com>
2026-04-21 08:51:35 -0600

    peticion post

---
+++ Consumo APIs/postApi.ino
@@ -0,0 +1,12 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid ="ALUMNOS TEC";
+const char* password ="";
+
+
+void setup(){}
+
+void loop(){}
